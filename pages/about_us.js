ec6dcffb82775690ff570f73c1f0c09c3b774c7c
Srushti <srushti1010shah@gmail.com>
2022-02-09 17:10:21 +0530

    changed about component name

--- pages/about_us.js
+++ pages/about_us.js
@@ -1,17 +1,19 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Navbar from "../components/Navbar";
 
-class about extends Component {
-    render() {
+class About extends Component {
+  render() {
     return (
-        <div>
-            <Navbar />
-            <div className="  ">
-            <h1 className="px-20 font-sans text-3xl font-bold text-gray-600">About Us</h1>
-            </div>
+      <div>
+        <Navbar />
+        <div className="  ">
+          <h1 className="px-20 font-sans text-3xl font-bold text-gray-600">
+            About Us
+          </h1>
         </div>
+      </div>
     );
-    }
+  }
 }
 
-export default about;
+export default About;
