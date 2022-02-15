963d614561e384cc6158cfad8039267e31aab47d
Khushi Kothari <admin@MacBook-Air.local>
2022-02-16 02:07:03 +0530

    class to className

--- pages/index.js
+++ pages/index.js
@@ -30,76 +30,76 @@ export default function Home() {
         </div>
       </div>*/}
   
-      <section class="text-gray-600 body-font h-min-screen md:my-20">
-      <div class="container mx-auto flex px-5 py-10 md:py-24 md:flex-row flex-col items-center">
-        <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-cyan-600">About Us
-            <br class="hidden lg:inline-block" />
+      <section className="text-gray-600 body-font h-min-screen md:my-20">
+      <div className="container mx-auto flex px-5 py-10 md:py-24 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-cyan-600">About Us
+            <br className="hidden lg:inline-block" />
           </h1>
-          <p class="mb-8 leading-relaxed">Furrier is an Animal Welfare platform and an Information Hub for current and prospective pet owners, which aims to assist the owners in taking better care for their pets, by connecting them with essential facilities/services and by providing services like health/medication monitoring, diet plans for pets etc. Furthermore, we also aim to Promote and assist adoption of animals and contribute towards other animal welfare programs as well as create awareness regarding prominent issues of Animal Welfare, and propose possible solutions</p>
-          <div class="flex justify-center">
+          <p className="mb-8 leading-relaxed">Furrier is an Animal Welfare platform and an Information Hub for current and prospective pet owners, which aims to assist the owners in taking better care for their pets, by connecting them with essential facilities/services and by providing services like health/medication monitoring, diet plans for pets etc. Furthermore, we also aim to Promote and assist adoption of animals and contribute towards other animal welfare programs as well as create awareness regarding prominent issues of Animal Welfare, and propose possible solutions</p>
+          <div className="flex justify-center">
           <Link href="/about_us"><a className=" text-center border-r-2 border-b-2 border-l-2 border-gray-200 bg-white shadow-md rounded-r-full rounded-l-full p-4 font-bold text-cyan-600 text-md hover:shadow-lg w-full md:w-40">View Page</a></Link>
           </div>
         </div>
-        <div class="lg:max-w-lg lg:w-1/2 md:w-1/2">
-        <img class="object-cover object-center rounded" alt="hero" src="/about-us.png"></img>
+        <div className="lg:max-w-lg lg:w-1/2 md:w-1/2">
+        <img className="object-cover object-center rounded" alt="hero" src="/about-us.png"></img>
         </div>
       </div>
     </section>
 
-    <section class="text-gray-600 body-font h-min-screen md:my-20 bg-gray-100">
-      <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div class="hidden md:inline-block lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <img class="object-cover object-center rounded" alt="hero" src="/services.png" />
+    <section className="text-gray-600 body-font h-min-screen md:my-20 bg-gray-100">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="hidden md:inline-block lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <img className="object-cover object-center rounded" alt="hero" src="/services.png" />
         </div>
-        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-cyan-600">Services
-            <br class="hidden lg:inline-block" />
+        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-cyan-600">Services
+            <br className="hidden lg:inline-block" />
           </h1>
-          <p class="mb-8 leading-relaxed">We provide various services that are important for your pet. Check out our services and provide your pet with the best facilities available in the town.</p>
-          <div class="flex justify-center">
+          <p className="mb-8 leading-relaxed">We provide various services that are important for your pet. Check out our services and provide your pet with the best facilities available in the town.</p>
+          <div className="flex justify-center">
           <Link href="/services"><a className=" text-center border-r-2 border-b-2 border-l-2 border-gray-200 bg-white shadow-md rounded-r-full rounded-l-full p-4 font-bold text-cyan-600 text-md hover:shadow-lg w-full md:w-40">View Page</a></Link>
           </div>
         </div>
-        <div class="md:hidden lg:max-w-lg lg:w-1/2 md:w-1/2 mt-10 md:mt-0">
-          <img class="object-cover object-center rounded" alt="hero" src="/services.png" />
+        <div className="md:hidden lg:max-w-lg lg:w-1/2 md:w-1/2 mt-10 md:mt-0">
+          <img className="object-cover object-center rounded" alt="hero" src="/services.png" />
         </div>
       </div>
     </section>
 
-    <section class="text-gray-600 body-font h-min-screen md:my-20">
-      <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-cyan-600">Adoption
-            <br class="hidden lg:inline-block" />
+    <section className="text-gray-600 body-font h-min-screen md:my-20">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-cyan-600">Adoption
+            <br className="hidden lg:inline-block" />
           </h1>
-          <p class="mb-8 leading-relaxed">Check out the necessary guidelines required to adopt a pet to prevent yourself from any unknown legal troubles. Also, check out the 'Available Pets' section to know your options.</p>
-          <div class="flex justify-center">
+          <p className="mb-8 leading-relaxed">Check out the necessary guidelines required to adopt a pet to prevent yourself from any unknown legal troubles. Also, check out the 'Available Pets' section to know your options.</p>
+          <div className="flex justify-center">
           <Link href="/adoption/pets"><a className=" text-center border-r-2 border-b-2 border-l-2 border-gray-200 bg-white shadow-md rounded-r-full rounded-l-full p-4 font-bold text-cyan-600 text-md hover:shadow-lg w-full md:w-40">View Page</a></Link>
           </div>
         </div>
-        <div class="lg:max-w-lg lg:w-1/2 md:w-1/2">
-        <img class="object-cover object-center rounded" alt="hero" src="/adopt.jpeg"></img>
+        <div className="lg:max-w-lg lg:w-1/2 md:w-1/2">
+        <img className="object-cover object-center rounded" alt="hero" src="/adopt.jpeg"></img>
         </div>
       </div>
     </section>
 
-    <section class="text-gray-600 body-font h-min-screen md:py-20 bg-gray-100">
-      <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div class="hidden md:inline-block lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <img class="object-cover object-center rounded" alt="hero" src="/services.png" />
+    <section className="text-gray-600 body-font h-min-screen md:py-20 bg-gray-100">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="hidden md:inline-block lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <img className="object-cover object-center rounded" alt="hero" src="/services.png" />
         </div>
-        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-cyan-600">FAQs
-            <br class="hidden lg:inline-block" />
+        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-cyan-600">FAQs
+            <br className="hidden lg:inline-block" />
           </h1>
-          <p class="mb-8 leading-relaxed">Check out the FAQs in the Help Desk tab. Feel free to contact us in case of any further queries.</p>
-          <div class="flex justify-center">
+          <p className="mb-8 leading-relaxed">Check out the FAQs in the Help Desk tab. Feel free to contact us in case of any further queries.</p>
+          <div className="flex justify-center">
           <Link href="/help_desk"><a className=" text-center border-r-2 border-b-2 border-l-2 border-gray-200 bg-white shadow-md rounded-r-full rounded-l-full p-4 font-bold text-cyan-600 text-md hover:shadow-lg w-full md:w-40">View Page</a></Link>
           </div>
         </div>
-        <div class="md:hidden lg:max-w-lg lg:w-1/2 md:w-1/2 mt-10 md:mt-0">
-          <img class="object-cover object-center rounded" alt="hero" src="/services.png" />
+        <div className="md:hidden lg:max-w-lg lg:w-1/2 md:w-1/2 mt-10 md:mt-0">
+          <img className="object-cover object-center rounded" alt="hero" src="/services.png" />
         </div>
       </div>
     </section>
